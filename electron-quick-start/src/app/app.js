88bf1afca342f2bf98ff72ba7963f9a40cb8ff89
Subhas <subhas.sing@gmail.com>
2016-07-04 00:44:58 -0700

    framework

--- electron-quick-start/src/app/app.js
+++ electron-quick-start/src/app/app.js
@@ -1,3 +1,26 @@
+ var 
+    // browser window object
+    win = require('electron').remote.BrowserWindow ,
+
+    // os object
+    os = require('os'),
+
+    // path object
+    path = require('path'),
+
+    // fs object
+    fs = require('fs'),
+
+    // url object
+    url = require('url'),
+
+    // i18n module (translations)
+    i18n = require('i18n'),
+
+    //moment = require('moment'),
+
+    Q = require('q');
+
 // Global App skeleton for backbone
 var App = new Backbone.Marionette.Application();
 _.extend(App, {
@@ -9,14 +32,95 @@ _.extend(App, {
     Providers: {},
     Localization: {}
 });
+// Temporary ..
+App.dataPath = "/home/subhas/workspaces/tmpdata";
+// Special Debug Console Calls!
+win.log = console.log.bind(console);
+
+win.debug = function () {
+    var params = Array.prototype.slice.call(arguments, 1);
+    params.unshift('%c[%cDEBUG%c] %c' + arguments[0], 'color: black;', 'color: green;', 'color: black;', 'color: blue;');
+    console.debug.apply(console, params);
+};
+win.info = function () {
+    var params = Array.prototype.slice.call(arguments, 1);
+    params.unshift('[%cINFO%c] ' + arguments[0], 'color: blue;', 'color: black;');
+    console.info.apply(console, params);
+};
+win.warn = function () {
+    var params = Array.prototype.slice.call(arguments, 1);
+    params.unshift('[%cWARNING%c] ' + arguments[0], 'color: orange;', 'color: black;');
+    console.warn.apply(console, params);
+};
+win.error = function () {
+    var params = Array.prototype.slice.call(arguments, 1);
+    params.unshift('%c[%cERROR%c] ' + arguments[0], 'color: black;', 'color: red;', 'color: black;');
+    console.error.apply(console, params);
+    fs.appendFileSync(path.join(App.dataPath, 'logs.txt'), '\n\n' + (arguments[0].stack || arguments[0])); // log errors;
+};
+
+/*
+if (gui.App.fullArgv.indexOf('--reset') !== -1) {
+
+    var data_path = require('nw.gui').App.dataPath;
+
+    localStorage.clear();
+
+    fs.unlinkSync(path.join(data_path, 'data/watched.db'), function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+    fs.unlinkSync(path.join(data_path, 'data/movies.db'), function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+    fs.unlinkSync(path.join(data_path, 'data/bookmarks.db'), function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+    fs.unlinkSync(path.join(data_path, 'data/shows.db'), function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+    fs.unlinkSync(path.join(data_path, 'data/settings.db'), function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+}
+*/
+
+
 
 
 App.addRegions({
     Window: '.main-window-region'
 });
 
+var initTemplates = function () {
+    // Load in external templates
+    var ts = [];
+
+    _.each(document.querySelectorAll('[type="text/x-template"]'), function (el) {
+        var d = Q.defer();
+        $.get(el.src, function (res) {
+            el.innerHTML = res;
+            d.resolve(true);
+        });
+        ts.push(d.promise);
+    });
+
+    return Q.all(ts);
+};
+
 var initApp = function () {
-    var mainWindow = new App.View.MainWindow();
+    var mainWindow = new App.View.AppView();
+    //win.show();
 
     try {
         App.Window.show(mainWindow);
@@ -26,9 +130,11 @@ var initApp = function () {
 };
 
 App.addInitializer(function (options) {
-    initApp();
-        
+    initTemplates()
+        .then(initApp);
 });
+
+
 
 
 
---
+++ electron-quick-start/src/app/templates/main-window-tpl.tpl
@@ -0,0 +1,10 @@
+<section id="todoapp">
+    <header id="header">
+        <h1>Todos Hello</h1>
+        <input id="new-todo" class="new-todo" placeholder="What needs to be done?">
+        <button id="clear-all" >Clear All</button>
+    </header>
+    <section id="main">
+        <ul id="todo-list"></ul>
+    </section>
+</section>
