18666034eea2a3ec35d57d49b3001cfc0ed9fbce
Subhas <subhas.sing@gmail.com>
2016-07-14 20:01:12 -0700

    metadata added in chapter

--- electron-quick-start/src/app/config.js
+++ electron-quick-start/src/app/config.js
@@ -18,18 +18,25 @@ _.extend(App, {
         title: 'Test Apps',
         platform: process.platform,        
         basepath : '/home/subhas/dev_tools/VideoRepo',
-        chapters : ['chapter01','chapter02','chapter03'],
+        chapters : [{name:'chapter01',seq:1,description: "Test chapter 1",path:'' },
+                    {name:'chapter02',seq:2,description: "Test chapter 3",path:'' },
+                    {name:'chapter03',seq:3,description: "Test chapter 3",path:'' }],
 
         getContentPath : function(chapter) {
             var isValid = false;
+            var path = '';
             for(var i=0;i<this.chapters.length;i++){
-                if(this.chapters[i] == chapter){
+                if(this.chapters[i].name && this.chapters[i].name == chapter){
                     isValid = true;
+                    path = this.chapters[i].path;
                     break;
                 }
             }
             if(isValid){
-                return this.basepath +"/"+ chapter;
+                if(!path){
+                    path = chapter;
+                }
+                return this.basepath +"/"+ path;
             }
             return '';
         }  
