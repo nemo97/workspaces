349553f4dec4cbcc591d8cf720f586a704f55905
Subhas <subhas.sing@gmail.com>
2016-08-18 08:53:29 -0700

    web worker added

--- angular2-starter/systemjs.config.js
+++ angular2-starter/systemjs.config.js
@@ -10,7 +10,8 @@
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs',
     'h5webstorage':               'node_modules/h5webstorage',
-    'primeng':                    'node_modules/primeng'
+    'primeng':                    'node_modules/primeng',
+    'factorial': 'factorial'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
