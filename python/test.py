be93630a8526c7decabf2828cfc61661c6209a5f
Subhas <subhas.sing@gmail.com>
2017-05-25 18:20:50 -0700

    tet

---
+++ python/test.py
@@ -0,0 +1,15 @@
+f = file("test.txt",mode='r')
+#txt = f.read();
+#print txt
+# lines = f.readlines()
+
+# for line in lines:
+#     print "from loop =>"+line
+
+lineTest = filter(lambda x:x.)
+
+
+
+
+
+
