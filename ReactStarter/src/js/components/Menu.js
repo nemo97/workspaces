697e04e3854600dde2831138050ec0f57ff1a500
Subhas <subhas.sing@gmail.com>
2017-05-30 21:59:04 -0700

    changes

---
+++ ReactStarter/src/js/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class Menu extends React.Component{
+
+    render(){
+        console.log(" I am here ");
+
+        return <div>Menu</div>
+    }
+
+
+}
