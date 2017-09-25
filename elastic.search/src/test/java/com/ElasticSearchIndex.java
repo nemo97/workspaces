25d6c450cb96146e8454621bfa35bcc62a5acd08
subhas <subhas_sing@live.com>
2017-09-24 20:14:27 -0700

    gradle upgradle

--- elastic.search/src/test/java/com/ElasticSearchIndex.java
+++ elastic.search/src/test/java/com/ElasticSearchIndex.java
@@ -1,15 +1,52 @@
 package com;
 
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
+
+import java.net.InetAddress;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.junit.Assert.assertEquals;
 
 public class ElasticSearchIndex{
 
 
     @Test
-    public void test1(){
+    public void test1() throws  Exception {
 
 
+        TransportAddress address =
+                new InetSocketTransportAddress(
+                        InetAddress.getByName("192.168.1.148"), 9300);
+
+        Client client = new PreBuiltTransportClient(Settings.EMPTY)
+                            .addTransportAddress(address);
+
+
+        SearchResponse searchResponse = client
+                .prepareSearch("comicbook")
+                .setQuery(
+                    matchQuery("_all", "spider"))
+                .execute().actionGet();
+
+
+        SearchHit hit = searchResponse.getHits().getAt(0);
+        String food = hit.getSource().get("food").toString();
+
+        System.out.println("....resuts .."+food);
+
+         client.close();
     }
+
+
 
 }
---
+++ elastic.search/src/test/java/com/SearchES.java
@@ -0,0 +1,52 @@
+package com;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.util.HashMap;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+
+public class SearchES {
+
+
+    @Test
+    public void test1() throws  Exception {
+
+        RestClient restClient = RestClient.builder(
+                new HttpHost("192.168.1.148", 9200, "http")
+//                new HttpHost("localhost", 9201, "http"))
+                )
+                .build();
+
+        HttpEntity entity = new NStringEntity(
+                "{ \"query\": { \"match_all\": {}}}",
+                ContentType.APPLICATION_JSON);
+        // alternative: performRequestAsync
+        Response response = restClient.performRequest("POST",
+                "/_search", new HashMap<>(), entity);
+        String json = EntityUtils.toString(response.getEntity());
+
+        System.out.println("...json.."+json);
+
+        restClient.close();
+    }
+
+
+
+}
