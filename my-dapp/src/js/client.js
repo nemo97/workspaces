f9eeb5472a0b64a55ef7f8c3ca577cda4517f810
nemo97 <subhas.sing@gmail.com>
2017-11-20 23:46:24 -0700

    eth cleitn

--- my-dapp/src/js/client.js
+++ my-dapp/src/js/client.js
@@ -6,6 +6,12 @@ import HomeLayout from './pages/HomeLayout'
 
 const app = document.getElementById('app')
 
+// if (typeof web3 !== 'undefined') {
+//   web3 = new Web3(web3.currentProvider);
+// } else {
+  // set the provider you want from Web3.providers
+  
+// }
 ReactDOM.render(
   <HomeLayout />
 , app);
--- my-dapp/src/js/pages/HomeLayout.js
+++ my-dapp/src/js/pages/HomeLayout.js
@@ -1,6 +1,37 @@
 import React from "react"
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Web3 from "web3"
+
+let ethClient = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+const peopleContractABI  = [ { constant: true,
+                          inputs: [],
+                          name: 'getPersonList',
+                          outputs: [ [Object], [Object], [Object] ],
+                          payable: false,
+                          stateMutability: 'view',
+                          type: 'function' },
+                        { constant: true,
+                          inputs: [ [Object] ],
+                          name: 'personList',
+                          outputs: [ [Object], [Object], [Object] ],
+                          payable: false,
+                          stateMutability: 'view',
+                          type: 'function' },
+                        { constant: false,
+                          inputs: [ [Object], [Object], [Object] ],
+                          name: 'addPerson',
+                          outputs: [ [Object] ],
+                          payable: false,
+                          stateMutability: 'nonpayable',
+                          type: 'function' },
+                        { inputs: [],
+                          payable: false,
+                          stateMutability: 'nonpayable',
+                          type: 'constructor' } ]
+const PeopleContract = ethClient.eth.contract(peopleContractABI);
+const peopleContractInstance = PeopleContract.at("0xd234bf3fbb6622e810e5cca0b048cf29e9187a1b");
 
 export default class HomeLayout extends React.Component {
   constructor(props){
@@ -8,6 +39,15 @@ export default class HomeLayout extends React.Component {
     this.state = {
 
     }
+  }
+  
+  componentWillMount(){
+    console.log("from here")
+    console.log(ethClient);
+    window.ethClient = ethClient;
+    window.peopleContractABI = peopleContractABI;
+    window.PeopleContract = PeopleContract;
+    window.peopleContractInstance = peopleContractInstance;
   }
 
   // Add todo handler
